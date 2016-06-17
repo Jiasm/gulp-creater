ada79d0e8d17c8e9a7c3a5e0decb4ebb32626179
jiasm <jiashunming@outlook.com>
2016-06-17 11:01:36 +0800

    update test

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 'use strict';
 
-
+console.log('hello world');
 // npm i -D gulp gulp-sass gulp-babel babel-preset-es2015 gulp-concat gulp-rename gulp-imagemin gulp-uglify gulp-cssmin gulp-autoprefixer
